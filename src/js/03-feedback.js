fd852af080d4e9961298042c8e397235f8a52d6b
Julia Maykut <pankiulia@gmail.com>
2023-03-12 16:35:36 +0200

    three

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[type="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+const formState = { email: '', message: '' };
+const storageKey = 'feedback-form-state';
+
+const onFormInput = throttle(() => {
+  formState.email = emailInput.value;
+  formState.message = messageInput.value;
+  localStorage.setItem(storageKey, JSON.stringify(formState));
+}, 500);
+
+emailInput.addEventListener('input', onFormInput);
+messageInput.addEventListener('input', onFormInput);
+
+const savedState = localStorage.getItem(storageKey);
+
+if (savedState) {
+  const parsedState = JSON.parse(savedState);
+  formState.email = parsedState.email;
+  formState.message = parsedState.message;
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  localStorage.removeItem(storageKey);
+  form.reset();
+  console.log({
+    email: formState.email,
+    message: formState.message,
+  });
+});
