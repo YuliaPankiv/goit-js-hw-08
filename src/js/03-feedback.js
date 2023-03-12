964f166dad5f6be85a2ce6ed1a9c2c36c6b9e594
Julia Maykut <pankiulia@gmail.com>
2023-03-12 21:45:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,26 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-const emailInput = form.querySelector('input[name="email"]');
+const emailInput = form.querySelector('input[type="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
+const formState = { email: '', message: '' };
 const storageKey = 'feedback-form-state';
 
-// Функція для збереження стану форми в локальне сховище
-const saveFormState = throttle(() => {
-  const state = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem(storageKey, JSON.stringify(state));
+const onFormInput = throttle(() => {
+  formState.email = emailInput.value;
+  formState.message = messageInput.value;
+  localStorage.setItem(storageKey, JSON.stringify(formState));
 }, 500);
 
-// Функція для заповнення форми з локального сховища
-const loadFormState = () => {
-  const savedState = localStorage.getItem(storageKey);
-  if (savedState) {
-    const state = JSON.parse(savedState);
-    emailInput.value = state.email;
-    messageInput.value = state.message;
-  }
-};
+emailInput.addEventListener('input', onFormInput);
+messageInput.addEventListener('input', onFormInput);
 
-// Заповнюємо форму при завантаженні сторінки
-loadFormState();
+const savedState = localStorage.getItem(storageKey);
 
-// Відстежуємо зміни в полях форми і зберігаємо стан форми в локальне сховище
-form.addEventListener('input', saveFormState);
-
-// Очищуємо локальне сховище та форму при сабміті форми
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  localStorage.removeItem(storageKey);
-  emailInput.value = '';
-  messageInput.value = '';
-  console.log({
-    email: emailInput.value,
-    message: messageInput.value,
-  });
-});
+if (savedState) {
+  const parsedState = JSON.parse(savedState);
+  formState.email = parsedState.email;
+  formState.message = parsedState.message;
+  emailInput.value = parsedState.email;
+  messageInput.value = parsedState.message;
+}
